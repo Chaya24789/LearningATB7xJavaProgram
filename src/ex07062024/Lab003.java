8643452f1c21599db115e606dbb9114ba7d4f5dd
Chaya Ramesh <chaya24789@gmail.com>
2024-07-26 20:35:00 +0530

    all file

--- src/ex07062024/Lab003.java
+++ src/ex07062024/Lab003.java
@@ -2,6 +2,19 @@ package ex07062024;
 
 public class Lab003 {
     public static void main(String[] args) {
+        // single -line comment - this code will not run or executed
+        //2+2 = ?
+
+        //Purpose of comment
+        //Instruction to junior QA or Developer
+        // explain
+
+        /**
+         * multiple comment
+         * documentation purpose
+         *
+         *
+         */
 
 
     }
